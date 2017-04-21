9e602638c7c9f09f7b65835bf5a9fc129a9b7647
Arjun <arjungup10@gmail.com>
2017-04-21 03:15:33 -0700

    Created basic variables and constructor for Person

--- app/src/main/java/nautilussoup/tripplanner/Person.java
+++ app/src/main/java/nautilussoup/tripplanner/Person.java
@@ -1,7 +1,16 @@
 package nautilussoup.tripplanner;
 
+import android.location.Location;
+
 /**
  * Created by Arjun Gupta on 4/21/2017.
  */
 public class Person {
+    private String name;
+    private Location home;
+
+    public Person(String name) {
+        this.name = name;
+        //TO-DO: set location
+    }
 }
