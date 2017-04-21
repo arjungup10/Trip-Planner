fb3f153bd3f76f5f841555338be02af917da7954
Arjun <arjungup10@gmail.com>
2017-04-21 03:34:18 -0700

    Created basic variables and constructor for Trip

--- app/src/main/java/nautilussoup/tripplanner/Trip.java
+++ app/src/main/java/nautilussoup/tripplanner/Trip.java
@@ -1,8 +1,18 @@
 package nautilussoup.tripplanner;
 
+import android.location.Location;
+
 /**
  * Created by Arjun Gupta on 4/21/2017.
  */
 public class Trip {
+    private String tripName;
+    private Budget tripBudget;
+    private Location tripLocation;
 
+    public Trip(String name, double maxBudget, Location tripLocation) {
+        tripName = name;
+        tripBudget = new Budget(maxBudget);
+        //To-Do set location
+    }
 }
