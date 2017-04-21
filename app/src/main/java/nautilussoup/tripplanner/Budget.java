bca58ac0efcf2646110810ea230be636ca1a6063
Arjun <arjungup10@gmail.com>
2017-04-21 03:24:19 -0700

    Created basic variables and constructor for Budget

--- app/src/main/java/nautilussoup/tripplanner/Budget.java
+++ app/src/main/java/nautilussoup/tripplanner/Budget.java
@@ -1,7 +1,19 @@
 package nautilussoup.tripplanner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Arjun Gupta on 4/21/2017.
  */
 public class Budget {
+    private double maxBudget;
+    private double amountSpent;
+    private List payments;
+
+    public Budget(double maxBudget) {
+        this.maxBudget = maxBudget;
+        amountSpent = 0;
+        payments = new ArrayList<Payment>();
+    }
 }
