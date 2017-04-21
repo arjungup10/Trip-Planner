7cace1351435d1c3db70fac9020af6acf8b21625
Arjun <arjungup10@gmail.com>
2017-04-21 03:24:35 -0700

    Created basic variables and constructor for Payment

--- app/src/main/java/nautilussoup/tripplanner/Payment.java
+++ app/src/main/java/nautilussoup/tripplanner/Payment.java
@@ -4,4 +4,14 @@ package nautilussoup.tripplanner;
  * Created by Arjun Gupta on 4/21/2017.
  */
 public class Payment {
+    private Person personPaying;
+    private double amount;
+    private String description;
+
+    public Payment(Person personPaying, double amount, String description) {
+        this.personPaying = personPaying;
+        this.amount = amount;
+        this.description = description;
+    }
+
 }
