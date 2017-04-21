c4568ed6348d0185f7b56be0e5d7bd8348a3b646
Arjun <arjungup10@gmail.com>
2017-04-21 03:31:05 -0700

    Added basic variables and constructor to Event

--- app/src/main/java/nautilussoup/tripplanner/Event.java
+++ app/src/main/java/nautilussoup/tripplanner/Event.java
@@ -1,8 +1,20 @@
 package nautilussoup.tripplanner;
 
+import android.location.Location;
+
 /**
  * Created by Arjun Gupta on 4/21/2017.
  */
 public class Event {
+    private String eventName;
+    private String earliestStartTime;
+    private String latestEndTime;
+    private Location eventLocation;
 
+    public Event(String name, int startHour, int startMin, int endHour, int endMin, Location eventLocation) {
+        eventName = name;
+        earliestStartTime = String.valueOf(startHour).toString() + ":" + String.valueOf(startMin).toString();
+        latestEndTime = String.valueOf(endHour).toString() + ":" + String.valueOf(endMin).toString();
+        //TO-DO set location
+    }
 }
