51fdb8d290008ebdfa1a8e9f9394b764f48dafe4
Arjun <arjungup10@gmail.com>
2017-04-21 03:36:31 -0700

    Created tripMembers variable in Trip

--- app/src/main/java/nautilussoup/tripplanner/Trip.java
+++ app/src/main/java/nautilussoup/tripplanner/Trip.java
@@ -2,6 +2,9 @@ package nautilussoup.tripplanner;
 
 import android.location.Location;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Arjun Gupta on 4/21/2017.
  */
@@ -9,10 +12,12 @@ public class Trip {
     private String tripName;
     private Budget tripBudget;
     private Location tripLocation;
+    private List tripMembers;
 
     public Trip(String name, double maxBudget, Location tripLocation) {
         tripName = name;
         tripBudget = new Budget(maxBudget);
         //To-Do set location
+        tripMembers = new ArrayList<Person>();
     }
 }
