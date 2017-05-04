1fae3b54de7b517f8ec038ded8914819570a4587
Arjun <arjungup10@gmail.com>
2017-05-04 15:47:02 -0700

    Added Menu to TripActivity

--- app/src/main/java/nautilussoup/tripplanner/CreateTripActivity.java
+++ app/src/main/java/nautilussoup/tripplanner/CreateTripActivity.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.support.v7.widget.Toolbar;
 
--- app/src/main/java/nautilussoup/tripplanner/TripActivity.java
+++ app/src/main/java/nautilussoup/tripplanner/TripActivity.java
@@ -1,15 +1,15 @@
 package nautilussoup.tripplanner;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,27 +19,29 @@ public class TripActivity extends AppCompatActivity {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private List<Trip> trips;
-    Toast toast;
+    Toolbar myToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        toast = Toast.makeText(getApplicationContext(), "Hello toast!", Toast.LENGTH_SHORT);
         trips = new ArrayList<Trip>();
         trips.add(new Trip("Yosemite", 200.0));
         trips.add(new Trip("Tahoe", 100));
         trips.add(new Trip("Rafting", 100));
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_createevent);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(myToolbar);
         setContentView(R.layout.activity_trip);
-        mRecyclerView = (RecyclerView) findViewById(R.id.rvEvents);
 
+        // create relevant toolbar
+        myToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        setTitle("Trips");
+
+
+        //create the recyclerview
+        mRecyclerView = (RecyclerView) findViewById(R.id.rvEvents);
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
         mRecyclerView.setHasFixedSize(true);
-
         // use a linear layout manager
         LinearLayoutManager llm = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(llm);
@@ -50,11 +52,34 @@ public class TripActivity extends AppCompatActivity {
     }
 
     public void createEvent(View view) {
-        toast.show();
         startActivity(new Intent(TripActivity.this, CreateTripActivity.class));
     }
 
     public void addTrip(Trip tripToAdd) {
         trips.add(tripToAdd);
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+//        if (id == R.id.) {
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.trips_menu, menu);
+        return true;
+    }
+
 }
