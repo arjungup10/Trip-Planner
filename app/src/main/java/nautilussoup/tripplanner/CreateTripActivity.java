2b3dff52f9c23b12123b218938aa1e2b04259766
Arjun <arjungup10@gmail.com>
2017-05-04 15:09:18 -0700

    Created classes to allow creation of a new Trip

---
+++ app/src/main/java/nautilussoup/tripplanner/CreateTripActivity.java
@@ -0,0 +1,47 @@
+package nautilussoup.tripplanner;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by arjun on 5/3/2017.
+ */
+
+public class CreateTripActivity extends AppCompatActivity {
+    TextView tripNameField, tripBudgetField;
+    Toolbar myToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createevent);
+        myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        TextView tripNameField = (TextView)findViewById(R.id.Title);
+        TextView tripBudgetField = (TextView)findViewById(R.id.Budget);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+//        if (id == R.id.) {
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
