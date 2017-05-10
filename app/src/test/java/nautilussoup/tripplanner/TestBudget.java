c747e9df86634bbc67ac5d94fc8dc42115b7e681
Arjun <arjungup10@gmail.com>
2017-05-10 14:41:53 -0700

    Created Unit Test for Budget

---
+++ app/src/test/java/nautilussoup/tripplanner/TestBudget.java
@@ -0,0 +1,29 @@
+package nautilussoup.tripplanner;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class TestBudget {
+    private double maxBudget = 234.42;
+    private double epsilon = 0.0001;
+    private Budget testBudget = new Budget(maxBudget);
+
+    @Test
+    public void initMaxBudget() throws Exception {
+        assertEquals(maxBudget, testBudget.getMaxBudget(), epsilon);
+    }
+
+    @Test
+    public void initAmountPaid() throws Exception {
+        assertEquals(0.0, testBudget.getAmountSpent(), epsilon);
+    }
+
+    @Test
+    public void initPayments() throws Exception {
+        assertEquals(0, testBudget.getPayments().size());
+    }
+}
