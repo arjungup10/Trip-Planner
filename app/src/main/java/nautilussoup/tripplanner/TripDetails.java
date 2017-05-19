0ecf60abec1e4a5ce17c29282dd0e2734b9cbc6a
Arjun <arjungup10@gmail.com>
2017-05-18 20:34:19 -0700

    Created Bottom Navigation fragments and necessary changes

--- app/src/main/java/nautilussoup/tripplanner/TripDetails.java
+++ app/src/main/java/nautilussoup/tripplanner/TripDetails.java
@@ -1,13 +1,19 @@
 package nautilussoup.tripplanner;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class TripDetails extends AppCompatActivity {
+public class TripDetails extends AppCompatActivity implements
+        TripEventsFragment.OnFragmentInteractionListener,
+        TripItineraryFragment.OnFragmentInteractionListener,
+        TripPaymentsFragment.OnFragmentInteractionListener {
 
     private TextView mTextMessage;
 
@@ -16,18 +22,25 @@ public class TripDetails extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    selectedFragment = TripEventsFragment.newInstance();
                     mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
+                    selectedFragment = TripItineraryFragment.newInstance();
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
+                    selectedFragment = TripPaymentsFragment.newInstance();
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
-            return false;
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.tripDetails, selectedFragment);
+            transaction.commit();
+            return true;
         }
 
     };
@@ -40,6 +53,14 @@ public class TripDetails extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.tripDetails, TripItineraryFragment.newInstance());
+        transaction.commit();
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        
+    }
 }
