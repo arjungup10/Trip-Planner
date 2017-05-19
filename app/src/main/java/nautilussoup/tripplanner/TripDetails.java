23f0ff6b97dc1fb97b789c3d0c48128021191136
Arjun <arjungup10@gmail.com>
2017-05-19 00:06:56 -0700

    Modified bottom bar to fit application

--- app/src/main/java/nautilussoup/tripplanner/TripDetails.java
+++ app/src/main/java/nautilussoup/tripplanner/TripDetails.java
@@ -30,17 +30,14 @@ public class TripDetails extends AppCompatActivity implements
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             Fragment selectedFragment = null;
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.navigation_events:
                     selectedFragment = TripEventsFragment.newInstance();
-                    mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.navigation_itinerary:
                     selectedFragment = TripItineraryFragment.newInstance();
-                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
-                case R.id.navigation_notifications:
+                case R.id.navigation_payments:
                     selectedFragment = TripPaymentsFragment.newInstance();
-                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
