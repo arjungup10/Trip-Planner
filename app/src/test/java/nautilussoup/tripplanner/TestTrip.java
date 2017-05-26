64c5dab3016fa44f31875c5c34a9a95304b4c4d7
Arjun <arjungup10@gmail.com>
2017-05-26 12:35:11 -0700

    Added possible tests for Trip

---
+++ app/src/test/java/nautilussoup/tripplanner/TestTrip.java
@@ -0,0 +1,75 @@
+package nautilussoup.tripplanner;
+
+import android.location.Location;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import nautilussoup.tripplanner.Models.Budget;
+import nautilussoup.tripplanner.Models.Event;
+import nautilussoup.tripplanner.Models.Person;
+import nautilussoup.tripplanner.Models.Trip;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestTrip {
+    private String tripName = "Yosemite";
+    private double tripMaxBudget = 264.45;
+    private Location tripLocation = new Location("");
+    private ArrayList tripMembers = new ArrayList();
+    private ArrayList events = new ArrayList();
+    private Trip testTrip = new Trip(tripName, tripMaxBudget);
+    private double epsilon = .0001;
+
+    @Test
+    public void testAddMember() {
+        String name = "Larry Hu";
+        testTrip.addMember(name);
+        assertEquals(name, ((Person)testTrip.getTripMembers().get(0)).getName());
+    }
+
+    @Test
+    public void testAddEventOnlyName() {
+        String name = "Birthday Party";
+        Event testEvent = new Event(name);
+        assertEquals(name, testEvent.getName());
+    }
+
+    @Test
+    public void testAddEvent() {
+        String name = "Birthday Party";
+        testTrip.addEvent(name, 2017, 11, 30, 18, 30, 2017, 11, 30, 22, 30);
+        assertEquals(name, ((Event) testTrip.getEvents().get(0)).getName());
+    }
+
+
+    @Test
+    public void testGetTripName() {
+        assertEquals(tripName, testTrip.getTripName());
+    }
+
+    @Test
+    public void testGetTripBudget() {
+        assertEquals(tripMaxBudget, testTrip.getTripBudget().getMaxBudget(), epsilon);
+    }
+
+    @Test
+    public void testGetTripMembers() {
+        String name = "Larry Hu";
+        testTrip.addMember(name);
+        assertEquals(1, testTrip.getTripMembers().size());
+    }
+
+
+    @Test
+    public void testGetEvents() {
+        String name = "Birthday Party";
+        testTrip.addEvent(name, 2017, 11, 30, 18, 30, 2017, 11, 30, 22, 30);
+        assertEquals(1, testTrip.getEvents().size());
+    }
+
+
+}
