c9ed8d2f63aec71fc6f0c4025dade729b47e78b4
Arjun <arjungup10@gmail.com>
2017-06-02 18:13:23 -0700

    Some timepicker initializations

--- app/src/main/java/nautilussoup/tripplanner/Views/CreateEventActivity.java
+++ app/src/main/java/nautilussoup/tripplanner/Views/CreateEventActivity.java
@@ -7,13 +7,26 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.SimpleTimeZone;
+import java.util.TimeZone;
+
 import nautilussoup.tripplanner.R;
 
-public class CreateEventActivity extends AppCompatActivity {
+public class CreateEventActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
     TextView eventNameField;
+    Button eventStartTime;
+    TimePickerDialog startTime;
     Toolbar myToolbar;
 
     @Override
@@ -29,6 +42,43 @@ public class CreateEventActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         eventNameField = (TextView)findViewById(R.id.Title);
+        eventStartTime = (Button)findViewById(R.id.eventStartTime);
+
+
+        // Date and Time Setup
+        // get the supported ids for GMT-08:00 (Pacific Standard Time)
+        String[] ids = TimeZone.getAvailableIDs(-8 * 60 * 60 * 1000);
+        // if no ids were returned, something is wrong. get out.
+        if (ids.length == 0)
+            System.exit(0);
+
+        // create a Pacific Standard Time time zone
+        SimpleTimeZone pdt = new SimpleTimeZone(-8 * 60 * 60 * 1000, ids[0]);
+
+        // set up rules for Daylight Saving Time
+        pdt.setStartRule(Calendar.APRIL, 1, Calendar.SUNDAY, 2 * 60 * 60 * 1000);
+        pdt.setEndRule(Calendar.OCTOBER, -1, Calendar.SUNDAY, 2 * 60 * 60 * 1000);
+
+        // create a GregorianCalendar with the Pacific Daylight time zone
+        // and the current date and time
+        Calendar c = new GregorianCalendar(pdt);
+        Date trialTime = new Date();
+        c.setTime(trialTime);
+        int hour = c.get(c.HOUR_OF_DAY);
+        int minute = c.get(c.MINUTE);
+        String meridian;
+        if (c.get(c.AM_PM) == 1) {
+            meridian = "AM";
+        } else {
+            meridian = "PM";
+        }
+        eventStartTime.setText(hour % 12 + ":" + minute + " " + meridian);
+
+
+        startTime = TimePickerDialog.newInstance(this, false);
+        startTime.setVersion(TimePickerDialog.Version.VERSION_2);
+
+
     }
 
     @Override
@@ -72,5 +122,24 @@ public class CreateEventActivity extends AppCompatActivity {
             setResult(RESULT_CANCELED, returnIntent);
         }
         super.finish();
+    }
+
+    @Override
+    public void onTimeSet(TimePickerDialog view, int hourOfDay, int minute, int second) {
+        String time = "You picked the following time: "+hourOfDay+"h"+minute+"m"+second;
+        //startTime.set
+        //timeTextView.setText(time);
+        String meridian;
+        if (hourOfDay < 11 || hourOfDay == 24) {
+            meridian = "AM";
+        } else {
+            meridian = "PM";
+        }
+        eventStartTime.setText(hourOfDay % 12 + ":" + minute + " " + meridian);
+    }
+
+    public void openTimePicker(View v) {
+        Toast.makeText(this, "yay make a timepicker now", Toast.LENGTH_SHORT).show();
+        startTime.show(getFragmentManager(), "TimepickerDialog");
     }
 }
