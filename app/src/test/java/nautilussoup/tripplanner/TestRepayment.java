438e15727237dca3f86d244db3aad2d7a7502f90
Arjun <arjungup10@gmail.com>
2017-06-09 19:24:15 -0700

    Init testRepayment

---
+++ app/src/test/java/nautilussoup/tripplanner/TestRepayment.java
@@ -0,0 +1,43 @@
+package nautilussoup.tripplanner;
+
+import org.junit.Test;
+
+import nautilussoup.tripplanner.Models.Person;
+import nautilussoup.tripplanner.Models.Repayment;
+
+public class TestRepayment {
+    private Person personPaying = new Person("Ernest Lei", "Arcadia, CA");
+    private double amount = 0;
+    private Repayment testRepayment = new Repayment(personPaying, amount);
+
+    @Test
+    public void testRepayment() {
+
+    }
+
+    @Test
+    public void testAddPersonReceiving() {
+
+    }
+
+    @Test
+    public void testAddToAmount() {
+
+    }
+
+    @Test
+    public void testGetPersonPaying() {
+
+    }
+
+    @Test
+    public void testGetPeoplePaying() {
+
+    }
+
+    @Test
+    public void testGetAmount() {
+
+    }
+
+}
